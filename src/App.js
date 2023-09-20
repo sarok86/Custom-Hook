becfa989b28a7792688bcc9ef4f7c16573a9df13
Sarok Dastkhosh <saruk.dastkhosh5186@gmail.com>
2023-09-20 09:37:00 +0330

    save data in localStorage and return to input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
 
-import Login from './components/Login';
-import Landing from './components/Landing';
+// import Login from './components/Login';
+// import Landing from './components/Landing';
+
+
+import useLocalStorage from './hooks/useLocalStorage'; 
 
 const App = () => {
 
-    const [toggle , setToggle] = useState(true)
+    // const [toggle , setToggle] = useState(true);
+
+    const [name , SetName] = useLocalStorage("name","")
 
     return (
         <div>
-            <button onClick={() => setToggle(!toggle) }>Toggle</button>
-            {toggle ? <Landing /> : <Login/>}
+            {/* <button onClick={() => setToggle(!toggle) }>Toggle</button>
+            {toggle ? <Landing /> : <Login/>} */}
+
+            <input type='text' value={
+                name} onChange={event => SetName(event.target.value)}/>
         </div>
     );
 };
